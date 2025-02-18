0c4ebafd657b4bc5eb7393f07d865b5948889725
jh2020 <lgh0120ljh@gmail.com>
2025-02-18 18:46:00 +0900

    Feat: 하이퍼 파라미터 조정
    hsv_v도 추가할 예정

--- train_yolo.py
+++ train_yolo.py
@@ -4,10 +4,20 @@ import os
 # Load a model
 model = YOLO("/mnt/hdd_6tb/jh2020/yolo11n.pt")
 
+search_space={
+    "lr0": (1e-5, 1e-1),
+    "degrees": (0.0, 45.0),
+}
+
 # Train the model
-train_results = model.train(
+train_results = model.tune(
     data="/mnt/hdd_6tb/jh2020/plateYOLO11n.yaml",
-    epochs=50,
-    imgsz=640, 
-    device=0,
+    epochs=130,
+    iterations=300,
+    optimizer="AdamW",
+    space = search_space,
+    plots=False,
+    save=False,
+    val=False,
+    imgsz=640,
 )
