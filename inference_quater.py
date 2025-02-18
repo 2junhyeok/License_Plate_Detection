53022968187112542b33eec3d468b6e86440af78
jh2020 <lgh0120ljh@gmail.com>
2025-02-18 15:42:06 +0900

    WIP: resized image도 inference에 포함

--- inference_quater.py
+++ inference_quater.py
@@ -2,10 +2,32 @@ from PIL import Image, ImageDraw
 import torch
 import re
 import os
+import glob
+import natsort
+import data_utils
 from ultralytics import YOLO
 from PIL import ImageFont
 
 class InferenceQuater:
+    class boxes:
+        '''
+        YOLO's bbox 출력형식
+            result.boxes.xyxy
+            result.boxes.xywh
+        '''
+        def __init__(self, data):
+            self.data = data
+            self.xyxy = None
+            self.xywh = None
+        
+        def xyxy(self):
+            #self.xyxy = 
+            return self.xyxy
+        
+        def xywh(self):
+            #self.xywh = 
+            return self.xywh
+            
     def __init__(self, margin, model):
         self.margin = margin
         self.model = model
@@ -16,6 +38,10 @@ class InferenceQuater:
     def quater_crop(self, image_path):
         '''
         이미지에 마진을 고려한 4분할을 진행
+        Args:
+            image_path: 대상 이미지 경로
+        Return:
+            잘린 이미지 4장과 resize된 원본 이미지
         '''
         self.img = Image.open(image_path)
         w = self.img.size[0]
@@ -30,13 +56,15 @@ class InferenceQuater:
         quater3 = self.img.crop((0, h_hlaf-(self.y_margin/2), w_half+(self.x_margin/2), h))
         quater4 = self.img.crop((w_half+(self.x_margin/2), h_hlaf+(self.y_margin/2), w, h))
 
-        return quater1, quater2, quater3, quater4
+        resized_img = data_utils.image_resize(self.img)
+        
+        return quater1, quater2, quater3, quater4, resized_img
 
-    def infer_quater(self, args):
+    def infer_batch(self, args):
         '''
-        4개의 분할된 이미지에 대한 batch inference를 진행
+        4개의 분할된 이미지와 resized 이미지에 대한 batch inference를 진행
         Args:
-            *args: crop된 이미지 4장
+            *args: crop된 이미지 4장과 resized 이미지지
         Returns:
             각 inference를 담은 list
         '''
@@ -69,17 +97,23 @@ class InferenceQuater:
         '''
         원본 이미지 스케일에 맞게 조정
         Args:
-            *args: bbox list
+            *args: bbox list [x1, y1, w, h, lb, conf]
         '''
-        bboxes1, bboxes2, bboxes3, bboxes4 = args
+        bboxes1, bboxes2, bboxes3, bboxes4, bboxes5 = args
 
-        bboxes2[:,0] += self.img.size[0]/2 -self.x_margin/2 # x 방향 -margin
+        bboxes2[:,0] += self.img.size[0]/2 -self.x_margin/2# x 방향 -margin
+        
         bboxes3[:,1] += self.img.size[1]/2 -self.y_margin/2# y 방향 -margin
         
-        bboxes4[:,0] += self.img.size[0]/2 -self.x_margin/2# x 방향 -margin
-        bboxes4[:,1] += self.img.size[1]/2 -self.y_margin/2# y 방향 -margin
-            
-        return torch.cat([bboxes1, bboxes2, bboxes3, bboxes4], dim=0)
+        bboxes4[:,0] += self.img.size[0]/2 +self.x_margin/2# x 방향 -margin
+        bboxes4[:,1] += self.img.size[1]/2 +self.y_margin/2# y 방향 -margin
+        
+        bboxes5[:,0] *= self.img.size[0]/640
+        bboxes5[:,1] *= self.img.size[1]**2/640*self.img.size[0]
+        bboxes5[:,2] *= self.img.size[0]/640
+        bboxes5[:,3] *= self.img.size[1]**2/640*self.img.size[0]
+        
+        return torch.cat([bboxes1, bboxes2, bboxes3, bboxes4, bboxes5], dim=0)
     
     @staticmethod
     def batch_iou(box, boxes):
@@ -147,7 +181,7 @@ class InferenceQuater:
         '''
         img = self.img.copy()
         bboxes = bboxes.to('cpu').numpy()
-        font = ImageFont.load_default(size=30)
+        font = ImageFont.load_default(size=20)
         
         draw = ImageDraw.Draw(img)
         for bbox in bboxes:
@@ -162,23 +196,39 @@ class InferenceQuater:
             
         return img
     
-    def __call__(self, image_path):
+    def infer(self, image_path):
         '''
         최종 처리된 이미지를 return함
         '''
         args = self.quater_crop(image_path)
-        results = self.infer_quater(args)
+        results = self.infer_batch(args)
         bboxes= self.extract_xywh(results)
         bboxes= self.scaling(bboxes)
         bboxes= InferenceQuater.nms(bboxes)
         image = self.plot(bboxes)
         return image
+
+    def __call__(self, image):
+        '''
+        result 와 같은 결과가 나오도록 해야함
+        Args:
+            image: PIL
+        '''
+        
+        return result
+        
     
 
 if __name__=="__main__":
-    model = YOLO("/mnt/hdd_6tb/jh2020/runs/detect/train28/weights/best.pt")
+    model = YOLO("/mnt/hdd_6tb/jh2020/runs/detect/tune/weights/best.pt")
     custom_model = InferenceQuater(margin=0.1, model=model)
-    org_img_path = "/mnt/hdd_6tb/jh2020/pred/image32.png"
-    img = custom_model(org_img_path)
-    img.save('test_quater.png', 'png')
-    
+    #org_img_path = "/mnt/hdd_6tb/jh2020/pred/image58.png"
+    #img = custom_model.infer(org_img_path)
+    #img.save('test_quater.png', 'png')
+
+    img_path = "/mnt/hdd_6tb/jh2020/processed_test/images"
+    save_path = "/mnt/hdd_6tb/jh2020/runs/detect/predict5"
+    img_path_lst = glob.glob(img_path+'/**/*.png', recursive=True)
+    for i,path in enumerate(natsort.natsorted(img_path_lst)):
+        img = custom_model.infer(path)
+        img.save(f"{save_path}/image{i}.png", "png")
