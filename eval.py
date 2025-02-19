59e7c33af8bdfa6dafd1e5b6037eab519525d06f
jh2020 <lgh0120ljh@gmail.com>
2025-02-19 17:20:55 +0900

    [Fix] nms metric 변경
    기존 cos sim 기반 nms에서는 행렬 연산을 통한 속도개선을 목표로 하였으나
    좌표 상에서의 연산이라는 한계 때문에 정규화 과정에서
    고유한 bbox를 특정하지 못하는 문제가 발생하여 iou기반 nms로 변경

--- eval.py
+++ eval.py
@@ -4,64 +4,51 @@ from PIL import Image
 import natsort
 import glob
 from ultralytics import YOLO
+from inference_quater import QuaterYOLO
 
+device = "cuda:0"
 
-def pred_to_xyxyw(result):
+def matching(A: torch.tensor,B: torch.tensor, threshold=0.1):
     '''
-    xyxy,xywh -> xyxyw
+    iou nms matching list
+    Args:
+        param 'A': YOLO's result
+        param 'B': ground truth .txt
     '''
-    xywh_tensor = result.boxes.xywh
-    xyxy_tensor = result.boxes.xyxy
-    lst = []
-    for i in zip(xywh_tensor, xyxy_tensor):
-        new = torch.Tensor((i[0][0],i[0][1],i[0][2]+i[1][2],i[0][1]+i[1][3],i[0][2])).to('cpu').numpy()
-        lst.append(new)
-    return np.array(lst)
-
-def label_to_xyxyw(label):
-    '''
-    xywh -> xyxyw
-    '''
-    lst = []
-    for i in label:
-        new = torch.Tensor((i[0],i[1],i[0]+i[2],i[1]+i[3],i[2])).to('cpu').numpy()
-        lst.append(new)
-    return np.array(lst)
-
-def cos_sim(A: np.ndarray,B: np.ndarray)->np.ndarray:
-    dot_product = A @ B.T
-    norm_A = np.linalg.norm(A, axis=1)[:,np.newaxis]
-    norm_B = np.linalg.norm(B, axis=1)[np.newaxis,:]
-    sim_mat = dot_product/(norm_A @ norm_B)
-    return sim_mat
-
-
-def matching(A: np.ndarray,B: np.ndarray, threshold=0.9)-> list:
-    '''
-    cosine similarity matrix to matching list
-    :param 'A': YOLO's result
-    :param 'B': ground truth .txt
-    '''
-    gt_lst = []
-    pred_lst = []
-    tmp_mat = cos_sim(A,B) # rank mat
-    while tmp_mat.max() > threshold:
-        i,j = np.where(tmp_mat==tmp_mat.max())
-        i,j = i[0],j[0]
+    if len(A)>0:# num pred > 0
+        iou_mat = torch.stack([QuaterYOLO.batch_iou(a, B) for a in A], dim=0)
+        '''
+        for a in A:
+            ious = QuaterYOLO.batch_iou(a, B)
+            iou_mat.append(ious)
+        iou_mat = torch.stack(iou_mat, dim=0)
+        '''
+        matched_A = []
+        matched_B = []
         
-        tmp_mat[i,:] = 0
-        tmp_mat[:,j] = 0
-        gt_lst.append(A[i.item()].tolist())
-        pred_lst.append(B[j.item()].tolist())
-    return pred_lst, gt_lst# matching list
+        while iou_mat.max() > threshold:
+            i,j = torch.where(iou_mat == iou_mat.max())
+            i = i[0].item()
+            j = j[0].item()
+            
+            matched_A.append(A[i])
+            matched_B.append(B[j])
+            
+            iou_mat[i,:] = 0
+            iou_mat[:,j] = 0
+    elif len(A) == 0:
+        matched_A = []
+        matched_B = []
+        
+    return matched_A, matched_B
 
-
-
-def eval(img_path,gt_path, model):
+def eval(img_path,gt_path, model, mode='dev'):
     '''
     img_path, model -> pred
     gt, pred -> matching
     matcing -> recall score
+    Args:
+        mode: 'base', 'dev'
     '''
     img_path_lst = glob.glob(img_path+'/**/*.png', recursive=True)
     gt_path_lst = glob.glob(gt_path+'**/*.txt', recursive=True)
@@ -71,31 +58,34 @@ def eval(img_path,gt_path, model):
     TP = 0
     TP_FN = 0
     for img_path, gt_path in zip(img_path_lst, gt_path_lst):
-        img = Image.open(img_path)
-        result = model(img, save=True)
+        
+        if mode=='base':
+            img = Image.open(img_path)
+            result = model(img, save=True)
+        elif mode=='dev':
+            result = model(img_path)
         
         with open(gt_path) as gt_lst:
             lst = [list(map(float, line.split()[1:])) for line in gt_lst]
-        
-        A = pred_to_xyxyw(result[0])
-        B = label_to_xyxyw(lst)
-        try:
-            pred_lst, _ = matching(A, B)
-            TP += len(pred_lst)
+
+            if mode=='base':
+                A = result[0].boxes.xywh.to(device)
+            elif mode=='dev':
+                A = result.boxes.xywh.to(device)
+            
+            A = A[:, :4]
+            B = torch.stack([torch.tensor(i[:4]) for i in lst], dim=0).to(device) # xywh
+
+            matched_A, _ = matching(A, B)
+            TP += len(matched_A)
             TP_FN += len(B)
-        except:# num_pred==0 or num_ground truth==0
-            print(f"num_pred: {len(pred_lst)}")
-            print(f"num_ground truth: {len(B)}")
-            pass
+            
     return TP/TP_FN
-        
-        
-        
-        
+
 
 if __name__=="__main__":
     test_img_path = "/mnt/hdd_6tb/jh2020/processed_test/images"
     test_label_path = "/mnt/hdd_6tb/jh2020/processed_test/labels" # gt
-    model = YOLO("/mnt/hdd_6tb/jh2020/runs/detect/train28/weights/best.pt")
-
-    print("recall score: ", eval(test_img_path, test_label_path, model))
+    model = YOLO("/mnt/hdd_6tb/jh2020/runs/detect/tune/weights/best.pt")
+    #model = QuaterYOLO(margin=0.1, model=model)
+    print("recall score: ", eval(test_img_path, test_label_path, model, mode='base'))
