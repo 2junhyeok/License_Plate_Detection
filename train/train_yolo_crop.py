463c49f6e8ca600cf580350af03256169763e776
jh2020 <lgh0120ljh@gmail.com>
2025-02-20 18:31:19 +0900

    [Add] yolo_crop 학습 및 폴더 이동

---
+++ train/train_yolo_crop.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import os
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+# Load a model
+model = YOLO("/mnt/hdd_6tb/jh2020/yolo11n.pt")
+
+# Train the model
+train_results = model.train(
+    data="/mnt/hdd_6tb/jh2020/processed_plate/data.yaml",
+    epochs=100,
+    optimizer="AdamW",
+    plots=False,
+    save=False,
+    imgsz=640,
+)
